b434edf51b7f9942998f44297e4163efd0b004aa
t3ch4lch3m15t <tech.alchemist@outlook.com>
2021-10-23 02:43:42 +0530

    Installation file updated

--- install.sh
+++ install.sh
@@ -5,7 +5,10 @@
 
 sudo mkdir -p /home/LTNM/Data/WalletData /home/LTNM/Data/NodeData
 sudo -H chown -R ${USER}.${USER} /home/LTNM
-cp -par /opt/opsdude/ltnm-utils/Bins /home/LTNM/
-sudo cp -par /opt/opsdude/ltnm-utils/LTNM.conf /home/LTNM/LTNM.conf
+cd /home/LTNM/
+function gdrive_download () {   CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$1" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p');   wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$CONFIRM&id=$1" -O $2;   rm -rf /tmp/cookies.txt; }
+gdrive_download 14smRDg3VjKZ-L3fOT8DaMnVKsoSXaCGp Bins.tgz
+tar -xvzf Bins.tgz
+cp -par /opt/opsdude/ltnm-utils/LTNM.conf /home/LTNM/LTNM.conf
 
 ## E O F ##
