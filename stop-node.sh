d4bfbad3f5c2f870afe7c5ef61638623795fb6d6
t3ch4lch3m15t <tech.alchemist@outlook.com>
2021-10-23 02:49:30 +0530

    Stop node updated

--- stop-node.sh
+++ stop-node.sh
@@ -4,8 +4,10 @@
 VAR1="LTNM"
 VAR2="Bins"
 
-for PID in $(ps | aux | grep "${VAR1}" | grep "${VAR2}" | sed '/grep/d' | awk '{print $2}')
+for PID in $(ps aux | grep "${VAR1}" | grep "${VAR2}" | sed '/grep/d' | awk '{print $2}')
 do
 kill -9 $PID
 echo "[+] $PID Killed"
 done
+
+## E O F ##
