76dc1ed25a29f44f3cfb3ad396aa14e779a63882
t3ch4lch3m15t <tech.alchemist@outlook.com>
2021-10-23 02:45:26 +0530

    Start node updated

--- start-node.sh
+++ start-node.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ## Script to Manage BTC Latinum Node ##
 
-BINARY="/home/LTNM/Bins/Bitcoinlatinum-stakingd"
-CONFIG="/jome/LTNM/LTNM.conf"
+BINARY="/home/LTNM/Bins/BitcoinLatinum-stakingd"
+CONFIG="/home/LTNM/LTNM.conf"
 
 ${BINARY} -conf=${CONFIG}
