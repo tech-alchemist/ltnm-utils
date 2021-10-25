4c7884ab6c37b79dfee3c6a08bfe4283e14f9097
t3ch4lch3m15t <tech.alchemist@outlook.com>
2021-10-25 18:47:02 +0530

    Backup script stabilized upto a bit

--- backup.sh
+++ backup.sh
@@ -8,18 +8,17 @@ S3_PROFILE="ltnm-backups"
 INSTANCE_ID="$(ec2metadata --instance-id)"
 DATE="$(date +%d.%b.%Y_%I.%M.%p)"
 
+cd /home/LTNM/Data || { echo "[-] Data Directory Not found, Exiting..." ; exit 1 ; }
 backup_blocks(){
-DATA_DIR="/home/LTNM/Data/NodeData"
 BACKUP_FILE="${DATE}.${INSTANCE_ID}.NodeData.tgz"
-tar -cvzf ${BACKUP_FILE} ${DATA_DIR}
+tar -czf ${BACKUP_FILE} NodeData
 aws s3 --profile ${S3_PROFILE} cp ${BACKUP_FILE} s3://${BUCKET_NAME}/BlockChain/${INSTANCE_ID}/
 echo "Blocks Backup file pushed to s3://${BUCKET_NAME}/BlockChain/${INSTANCE_ID}/${BACKUP_FILE}"
 }
 
 backup_wallet(){
-DATA_DIR="/home/LTNM/Data/WalletData"
 BACKUP_FILE="${DATE}.${INSTANCE_ID}.WalletData.tgz"
-tar -cvzf ${BACKUP_FILE} ${DATA_DIR}
+tar -czf ${BACKUP_FILE} WalletData
 aws s3 --profile ${S3_PROFILE} cp ${BACKUP_FILE} s3://${BUCKET_NAME}/BlockChain/${INSTANCE_ID}/
 echo "Wallet Backup file pushed to s3://${BUCKET_NAME}/BlockChain/${INSTANCE_ID}/${BACKUP_FILE}"
 }
